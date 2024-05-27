fe62ec4583459a395f0431c4e428ba23ce15ffbe
HenokGm <henokgmariam7@gmail.com>
2024-05-27 08:40:50 +0300

    initial commite

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add {
+    
+    public static void main(String [] args){
+        int num1 = 4;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println("the Sum of the two Numbers is: " + result);
+    }
+}
---
+++ multiplication.java
@@ -0,0 +1,14 @@
+/**
+ * multiplication
+ */
+public class multiplication {
+
+    public double multiplication (){
+
+        double num1 = 3;
+        double num2 = 4;
+        double result = num1 * num2;
+
+        return result;
+    }
+}
